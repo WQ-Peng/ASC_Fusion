504e071f6f15cc7bca36948aca0664d139f9711a
Your Name <you@example.com>
2020-06-23 11:52:59 +0800

    fix download_dataset.sh(char)

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,12 +1,13 @@
 # download dataset and unzip
 
 #train_set
+cd ../
 mkdir dataset
 cd dataset
 mkdir train_set
 mkdir test_set
 
-for i in {1..10}
+for i in 1 2 3 4 5 6 7 8 9 10
 do
 wget https://zenodo.org/record/400515/files/TUT-acoustic-scenes-2017-development.audio.$i.zip?download=1
 unzip TUT-acoustic-scenes-2017-development.audio.$i.zip?download=1 -d train_set
